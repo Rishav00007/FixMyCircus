8d81fad0524209ac27a82de7a126d8cda2a86fd6
Ritik20092004 <ritiktiwari4703@gmail.com>
2025-10-06 13:09:04 +0530

    bug fix in complaintcontroller

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { createComplaint, getAllComplaints,getMyComplaints,updateComplaintStatus,deleteComplaint } from "../controllers/complaintController.js";
 import { upload } from "../middlewares/multer.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 const router = express.Router();
 
-router.post("/",upload.single("photo"),createComplaint);
-router.get("/",getAllComplaints);
-router.get("/my",getMyComplaints);
-router.put("/:id",updateComplaintStatus);
-router.delete("/:id",deleteComplaint);
+router.post("/",protect,upload.single("photo"),createComplaint);
+router.get("/",protect,authorizeRoles("admin","staff"),getAllComplaints);
+router.get("/my",protect,getMyComplaints);
+router.put("/:id",protect,authorizeRoles("admin","staff"),updateComplaintStatus);
+router.delete("/:id",protect,authorizeRoles("admin"),deleteComplaint);
 
 export default router;
