1922bb795a7733a388bea1aa4d1c3bf94ed239b2
Ritik20092004 <ritiktiwari4703@gmail.com>
2025-10-06 17:01:11 +0530

    fix bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,23 @@ import Login from "./components/Auth/Login";
 import ForgotPassword from "./components/Auth/ForgotPassword";
 import ResetPassword from "./components/Auth/ResetPassword";
 
-import { AuthContextProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <AuthContextProvider>
+    <AuthProvider>
       <Router>
         <Routes>
+          <Route path="/" element={<Home/>} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
         </Routes>
       </Router>
-    </AuthContextProvider>
+    </AuthProvider>
+    
   );
 };
 
